2b855de2142280f39923b5341dd163b8a8967f11
Suhas <suhas8706@gmail.com>
2024-10-02 19:34:45 +0530

    Added project

--- Projects/Job-Portal/frontend/src/App.jsx
+++ Projects/Job-Portal/frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> hello from suhas and akangshya</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,7 +24,6 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <h1>Hello this is my branch</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
